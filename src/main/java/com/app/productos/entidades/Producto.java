c4b51d089aebcc58e0e10d0d917468185979c144
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-08-16 13:09:38 -0600

    Creacion de entidad

---
+++ src/main/java/com/app/productos/entidades/Producto.java
@@ -0,0 +1,57 @@
+package com.app.productos.entidades;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "productos")
+public class Producto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(length = 64)
+    private String nombre;
+
+    private float precio;
+
+    public Producto() {
+
+    }
+
+    public Producto(Integer id, String nombre, float precio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Producto(String nombre, float precio) {
+        this.nombre = nombre;
+        this.precio = precio;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+}

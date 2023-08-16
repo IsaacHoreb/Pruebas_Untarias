3072125abf26513005aaf4d4c122545914f46661
Isaac Horeb Gonzalez Gamas <isaacgonzalezgamas20@gmail.com>
2023-08-16 13:03:08 -0600

    Creación del Proyecto

---
+++ src/test/java/com/app/productos/ProductosAppTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.productos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductosAppTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
